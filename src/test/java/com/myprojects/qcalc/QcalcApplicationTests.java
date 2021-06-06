53f1179081189c1202f608ba93c756692844660f
cheenmaya-bhutad <55523581+cheenmaya-bhutad@users.noreply.github.com>
2021-06-06 16:18:55 +0530

    Initial commit

---
+++ src/test/java/com/myprojects/qcalc/QcalcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myprojects.qcalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QcalcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
